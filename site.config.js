7f452d1d2137b6162545ef760c828e473bdb420f
ieuns <les070207@naver.com>
2026-03-17 17:34:48 +0900

    Update blog title and description in site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "ieuns",
+    description: "Welcome",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
